63b0cc347fed8920e0a5629605bd434afa41f635
hugoj-goncalves <hugo.goncalves@benner.com.br>
2017-05-18 08:33:30 -0300

    Runtests passando apenas bin/[FOLDER]

--- Bash/Conecta/configs.sh
+++ Bash/Conecta/configs.sh
@@ -2,10 +2,11 @@ CONECTA_DIR="/c/workspace/Conecta"
 ALTERNATIVE_CONECTA_DIR="/e/Compart/Conecta/Conecta"
 INFRA_DIR="/c/workspace/Benner.Infra"
 
-RELEASE_BRANCH='release/Sprint30'
+RELEASE_BRANCH='release/Sprint31'
 
 MSBUILD='/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe'
 IISEXPRESS=/c/Program\ Files/IIS\ Express/iisexpress.exe
+NUNIT_EXE=/c/Program\ Files\ \(x86\)/NUnit\ 2.6.4/bin/nunit-console.exe
 
 # Não alterar a não ser que saiba exatamente o que está fazendo.
 nuget='./.nuget/NuGet.exe'
