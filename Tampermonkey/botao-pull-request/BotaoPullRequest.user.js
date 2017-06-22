c4b10e9e61390f04b005ecb25bf3d4f8949e54eb
José Carlos S.A. Tissei <jose.tissei@benner.com.br>
2017-06-21 21:14:25 -0300

    Botão Pull Request no Detalhes da SMS (#12)
    * Adicionando Botao Pull Request
    
    * Adicionado imagens
    
    * Create README.md

---
+++ Tampermonkey/botao-pull-request/BotaoPullRequest.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Botão Pull Request Para SMS
+// @namespace    Benner.Saude
+// @version      0.0.1
+// @description  Adiciona botão para pull request da SMS
+// @author       José Carlos S. A. Tissei
+// @include      https://siscon.benner.com.br/siscon/e/solicitacoes/Solicitacao.aspx*
+// @grant GM_addStyle
+// @run-at document-end
+// ==/UserScript==
+
+(function() {  
+    function pesquisarPullRequest () {
+        var regex = new RegExp(/key=([0-9]+)/g);
+        var numeroSms = regex.exec(window.location.href)[1];
+        var linkPullRequest = "https://github.com/pulls?utf8=%E2%9C%93&q=is%3Aopen+is%3Apr+" + numeroSms + "+user%3Abennersaude";
+        
+        return "window.open('"+linkPullRequest+"', '_blank');";
+    };
+    
+    function montarBotaoPullRequest () {      
+        var botoes = document.getElementById("ctl00_Main_ucSolicitacao_WIDGETID_FORMSOLICITACAO_toolbar");
+        var icone = "<i class=\"fa fa-code-fork btn-action\"></i>";
+        var botao = "<a id=\"botao-pull-request\" href=\"javascript:" + pesquisarPullRequest() + "\" class=\"btn command-action custom-action blue\">" + icone + "</a>";
+        botoes.innerHTML = botao + botoes.innerHTML;
+    }
+
+    $(document).ready(function() {
+        montarBotaoPullRequest();
+    });
+})();
---
+++ Tampermonkey/botao-pull-request/README.md
@@ -0,0 +1,5 @@
+## Tela Detalhes da SMS
+
+Este script adiciona um botão que busca o pull request da sms no github, funciona para todos os repositorios da benner saúde:
+  ![Detalhes SMS](botao-pr.PNG)
+  ![Github](busca-github.PNG)
