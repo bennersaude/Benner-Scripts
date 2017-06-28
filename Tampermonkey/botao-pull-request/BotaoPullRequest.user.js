735c4a2844830634287c3ec3ee3a4ae5f5e071b2
Hugo José Gonçalves <hugo.goncalves@benner.com.br>
2017-06-28 13:37:35 -0300

    Olhando source branch ao inves do titulo (#15)

--- Tampermonkey/botao-pull-request/BotaoPullRequest.user.js
+++ Tampermonkey/botao-pull-request/BotaoPullRequest.user.js
@@ -1,25 +1,26 @@
 // ==UserScript==
 // @name         Botão Pull Request Para SMS
 // @namespace    Benner.Saude
-// @version      1.0
+// @version      1.1
 // @description  Adiciona botão para pull request da SMS
 // @author       José Carlos S. A. Tissei
+// @contributor  Hugo José Gonçalves
 // @include      https://siscon.benner.com.br/siscon/e/solicitacoes/Solicitacao.aspx*
 // @updateURL    https://github.com/bennersaude/Benner-Scripts/raw/master/Tampermonkey/botao-pull-request/BotaoPullRequest.user.js
 // @grant GM_addStyle
 // @run-at document-end
 // ==/UserScript==
 
-(function() {  
+(function() {
     function pesquisarPullRequest () {
         var regex = new RegExp(/key=([0-9]+)/g);
         var numeroSms = regex.exec(window.location.href)[1];
-        var linkPullRequest = "https://github.com/pulls?utf8=%E2%9C%93&q=is%3Aopen+is%3Apr+" + numeroSms + "+user%3Abennersaude";
-        
+        var linkPullRequest = "https://github.com/pulls?utf8=%E2%9C%93&q=is%3Apr+head%3A" + numeroSms + "+head%3Ahotfix%2F" + numeroSms + "+head%3ASMS%2F" + numeroSms + "+user%3Abennersaude";
+
         return "window.open('"+linkPullRequest+"', '_blank');";
-    };
-    
-    function montarBotaoPullRequest () {      
+    }
+
+    function montarBotaoPullRequest () {
         var botoes = document.getElementById("ctl00_Main_ucSolicitacao_WIDGETID_FORMSOLICITACAO_toolbar");
         var icone = "<i class=\"fa fa-code-fork btn-action\"></i>";
         var botao = "<a id=\"botao-pull-request\" href=\"javascript:" + pesquisarPullRequest() + "\" class=\"btn command-action custom-action blue\">" + icone + "</a>";
