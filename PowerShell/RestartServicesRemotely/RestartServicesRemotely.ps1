43511e280a0e1b70edb4474ceef11f35947debe0
José Tissei <jose.tissei@benner.com.br>
2017-07-27 11:22:27 -0300

    Script para reiniciar servicos em uma maquina pela rede

---
+++ PowerShell/RestartServicesRemotely/RestartServicesRemotely.ps1
@@ -0,0 +1,10 @@
+﻿Param(
+  [string[]]$hostNames,
+  [string[]]$services
+)
+
+foreach($hostName in $hostNames) {
+    foreach($service in $services) {
+	Restart-Service -InputObject $(Get-Service -Computer $hostName -Name $service)
+    }
+}
