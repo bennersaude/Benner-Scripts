4a08e7fbb00cfe37e66d8f63a6ed0d64f19f2f72
hugoj-goncalves <hugo.goncalves@benner.com.br>
2017-12-15 14:33:26 -0200

    ok

--- Conecta/replicateTissVersionCodeDiff.sh
+++ Conecta/replicateTissVersionCodeDiff.sh
@@ -4,17 +4,47 @@ my_dir="$(dirname "$0")"
 source "$my_dir/configs.sh"
 source "$my_dir/utils.sh"
 
-fromTISS="V321"
-toTISS="V331"
+# V321 V331 V332
+fromTISS="$1"
 
-filesToPatch=($(git status --porcelain *"$fromTISS"*.cs | sed s/^...//))
+# Generate Compact Versions
+fromTISSCompact=${fromTISS:0:1}${fromTISS:2:1}${fromTISS:4:1}
 
-# Patch replication
-for i in "${filesToPatch[@]}"
-do
-    # Replace parts of the path
-    fileToPatch=$(echo "$i" | sed s/$fromTISS/$toTISS/)
-    echo "$i > $fileToPatch"
-    code --diff "$i" "$fileToPatch"
+# Generate Options Array
+fromTISSOptions=()
+
+fromTISSOptions+=("$fromTISS")
+
+fromTISSOptions+=("V$fromTISSCompact")
+
+fromTISSOptions+=("Tiss$fromTISSCompact")
+
+function replicate() {
+    filesToPatch=($(git status --porcelain *"$fromTISS"*.cs | sed s/^...//))
+
+    # Patch replication
+    for i in "${filesToPatch[@]}"
+    do
+        # Replace parts of the path
+        fileToPatch=$(echo "$i" | sed s/$fromTISS/$1/)
+        echo "$i > $fileToPatch"
+
+        if [ -e "$fileToPatch" ]; then
+            /C/Program\ Files/Perforce/p4merge.exe "$i" "$fileToPatch" "$fileToPatch" "$fileToPatch"
+            #code --diff "$i" "$fileToPatch"
+        else
+            targetDir="$(dirname $fileToPatch/)"
+            mkdir -p "$targetDir"
+            cp "$i" "$targetDir"
+        fi
+
+    done
+}
+
+argc=$#
+argv=($@)
+
+for i in "${@:2}"; do
+    replicate "$i"
 done
 
