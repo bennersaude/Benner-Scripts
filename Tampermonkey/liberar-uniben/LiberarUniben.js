440e18ecde65c41f1676d021cd3633f306ed3303
Bruno Fernandes <bfernand@users.noreply.github.com>
2018-08-30 10:55:13 -0300

    Liberar uniben (#32)
    * Adição do script que faz solicitação de Liberação da Uniben
    
    * adicionado link de atualização automática do script
    
    * alterado Host para Não Definido de modo que dê aviso no primeiro acesso
    
    * ajustado nome das imagens no README.md

---
+++ Tampermonkey/liberar-uniben/LiberarUniben.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Liberar Uniben
+// @namespace    Benner.saude
+// @version      0.1
+// @description  Botão que abre o cliente de email com texto padrão e pessoas para solicitação de Liberação da Uniben
+// @author       Bruno Fernandes
+// @include      https://siscon.benner.com.br/siscon/e/solicitacoes/Atendimento.aspx*
+// @include      https://siscon.benner.com.br/siscon/e/solicitacoes/Solicitacao.aspx*
+// @updateURL    https://github.com/bennersaude/Benner-Scripts/raw/master/Tampermonkey/liberar-uniben/LiberarUniben.js
+// @grant        none
+// @run-at document-end
+// ==/UserScript==
+
+(function() {
+
+    function sendMail() {
+        var host = "Não Definido"                                                           // Alterar aqui seu host para liberação
+        if (host == "Não Definido"){ alert("Defina seu HOST no script \"Liberar Uniben\"") };
+        var regex = new RegExp(/key=([0-9]+)/g);
+        var numeroSms = regex.exec(window.location.href)[1];
+        var saudacao = "Boa noite!"
+        if (new Date().getHours() < 12){saudacao = "Bom dia!"}
+           else if (new Date().getHours() >= 12 && new Date().getHours() < 19) {saudacao = "Boa tarde!"}
+        var link = "mailto:alexiades.scalfo@unimedcuritiba.com.br;pereira@unimedcuritiba.com.br;claudemir.souza@unimedcuritiba.com.br;"
+             + "?&subject=" + escape("Liberar UNIBEN")
+             + "&body=" + escape(saudacao + "%0D%0A%0D%0A" + host + "%0D%0ASMS " + numeroSms + "%0D%0A%0D%0AAtenciosamente,")
+        ;
+
+        return "window.open('" + link + "', '_self'); ";
+}
+
+    function montarBotaoLiberarUniben() {
+        var botoes = document.getElementById("ctl00_Main_ucAtendimento_WIDGETID_FORMATENDIMENTO_toolbar");     //Protocolo
+        if (botoes == null){
+           botoes = document.getElementById("ctl00_Main_ucSolicitacao_WIDGETID_COMANDOS_SOLICITACAO_toolbar"); //SMS
+        }
+        var icone = "<i class=\"fa fa-mail-forward btn-action\"></i>";
+        var botao = "<a id=\"botao-liberar-uniben\" href=\"javascript:" + sendMail() + "\" class=\"btn command-action custom-action blue\" title=\"Liberar UNIBEN\" >" + icone + " </a>";
+        botoes.innerHTML = botoes.innerHTML + botao;
+    }
+
+    $(document).ready(function () {
+        montarBotaoLiberarUniben();
+        window.close();
+    });
+})();
